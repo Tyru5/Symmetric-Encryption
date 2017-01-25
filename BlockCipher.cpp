1d47ac7d9b40a5b4db7cdd9db384a3176c656703
Tyrus Malmstrom <tyrusm@hotmail.com>
2017-01-25 11:47:37 -0700

    Updated BlockCipher class and header file

--- BlockCipher.cpp
+++ BlockCipher.cpp
@@ -5,7 +5,9 @@
 // directives:
 #include <fstream>
 #include <sstream>
+#include <string.h>
 
+#include "BlockCipher.h"
 
 // namespace:
 using namespace std;
@@ -15,7 +17,32 @@ using namespace std;
 
 
 
+int BlockCipher::can_swap( const int& idx, char key[] ){
+  // cout << idx << endl;
+  return ( (int) key[idx] % 2 == 0 ) ? 0 : 1;
+}
 
+void BlockCipher::swap_chars( char string[], char key[] ){
 
+  char* sp =  &string[0];
+  char* ep = &string[ strlen(string) - 1];
+  
+  for( int i = 0; i < (int) strlen(string); i++){
 
+    if( *sp == *ep ) break;
+    
+    if( can_swap( i, key ) ){
+      // swap chars and increment pointers
+      char temp = *sp;
+      *sp = *ep;
+      *ep = temp;
+      sp++;
+      ep--;
+    }else{
+      sp++;
+    }
+    
+  }
+   
+}
 
--- BlockCipher.h
+++ BlockCipher.h
@@ -10,11 +10,34 @@
 #include <vector>
 
 
+using std::string;
 
 class BlockCipher {
 
  public:
 
+  // d-Constructor:
+  BlockCipher(){};
+
+  // v-Constructor:
+ BlockCipher(const string& inputfile_name_, const string& outputfile_name_, const string& key_):
+  inputfile_name( inputfile_name_ ),
+  outputfile_name( outputfile_name_ ),
+  key( key_ )
+  {};
+
+
+  // Member functions:
+  int can_swap( const int& idx, char key[] );
+  void swap_chars( char string[], char key[] );
+
+ protected:
+  
+  string inputfile_name;
+  string outputfile_name;
+
+  string key;
+  
   
 };
 
